88231dec184cb748c53828ba1064111871953c6f
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2023-10-04 13:05:52 -0500

    Run tests for private and public ECR registry in parallel

--- hack/iam/iam.sh
+++ hack/iam/iam.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Names for IAM policies and roles
+WEBHOOK_IAM_NAME="ecr-webhook-${GITHUB_JOB}"
+CREDENTIAL_HELPER_IAM_NAME="ecr-credential-helper-${GITHUB_JOB}"
+
+# Filepaths for IAM policies and roles
+WEBHOOK_POLICY_PATH="ecr-webhook-policy.json"
+WEBHOOK_ROLE_PATH="ecr-webhook-role.json"
+CREDENTIAL_HELPER_POLICY_PATH="ecr-credential-helper-policy.json"
+CREDENTIAL_HELPER_ROLE_PATH="ecr-credential-helper-role.json"
+
 create() {
     # Check if the cluster name argument is provided
     if [ -z "$2" ]; then
@@ -17,52 +27,52 @@ create() {
         exit 1
     fi
 
-    # Replace the placeholder in the JSON files with the AWS account ID and EKS cluster ID
+    # Replace the placeholder in the JSON role files with the AWS account ID and EKS cluster ID
     SED_CMD="sed"
     if [ "$(uname -s)" = "Darwin" ]; then
         SED_CMD="gsed"
     fi
-    "$SED_CMD" -i "s/{{AWS_ACCOUNT_ID}}/$AWS_ACCOUNT_ID/g" ecr-webhook-role.json
-    "$SED_CMD" -i "s/{{AWS_ACCOUNT_ID}}/$AWS_ACCOUNT_ID/g" ecr-credential-helper-role.json
+    "$SED_CMD" -i "s/{{AWS_ACCOUNT_ID}}/$AWS_ACCOUNT_ID/g" "$WEBHOOK_ROLE_PATH"
+    "$SED_CMD" -i "s/{{AWS_ACCOUNT_ID}}/$AWS_ACCOUNT_ID/g" "$CREDENTIAL_HELPER_ROLE_PATH"
 
-    "$SED_CMD" -i "s/{{EKS_CLUSTER_ID}}/$EKS_CLUSTER_ID/g" ecr-webhook-role.json
-    "$SED_CMD" -i "s/{{EKS_CLUSTER_ID}}/$EKS_CLUSTER_ID/g" ecr-credential-helper-role.json
+    "$SED_CMD" -i "s/{{EKS_CLUSTER_ID}}/$EKS_CLUSTER_ID/g" "$WEBHOOK_ROLE_PATH"
+    "$SED_CMD" -i "s/{{EKS_CLUSTER_ID}}/$EKS_CLUSTER_ID/g" "$CREDENTIAL_HELPER_ROLE_PATH"
 
     # Create IAM policies from JSON files
-    ECR_WEBHOOK_POLICY_ARN=$(aws iam create-policy --policy-name ecr-webhook --policy-document file://ecr-webhook-policy.json --query "Policy.Arn" --output text)
-    ECR_CREDENTIAL_HELPER_POLICY_ARN=$(aws iam create-policy --policy-name ecr-credential-helper --policy-document file://ecr-credential-helper-policy.json --query "Policy.Arn" --output text)
+    ECR_WEBHOOK_POLICY_ARN=$(aws iam create-policy --policy-name "$WEBHOOK_IAM_NAME" --policy-document "file://${WEBHOOK_POLICY_PATH}" --query "Policy.Arn" --output text)
+    ECR_CREDENTIAL_HELPER_POLICY_ARN=$(aws iam create-policy --policy-name "$CREDENTIAL_HELPER_IAM_NAME" --policy-document "file://${CREDENTIAL_HELPER_POLICY_PATH}" --query "Policy.Arn" --output text)
 
     # Create IAM roles from JSON files
-    ECR_WEBHOOK_ROLE_ARN=$(aws iam create-role --role-name ecr-webhook --assume-role-policy-document file://ecr-webhook-role.json --query "Role.Arn" --output text)
-    ECR_CREDENTIAL_HELPER_ROLE_ARN=$(aws iam create-role --role-name ecr-credential-helper --assume-role-policy-document file://ecr-credential-helper-role.json --query "Role.Arn" --output text)
+    ECR_WEBHOOK_ROLE_ARN=$(aws iam create-role --role-name "$WEBHOOK_IAM_NAME" --assume-role-policy-document "file://${WEBHOOK_ROLE_PATH}" --query "Role.Arn" --output text)
+    ECR_CREDENTIAL_HELPER_ROLE_ARN=$(aws iam create-role --role-name "$CREDENTIAL_HELPER_IAM_NAME" --assume-role-policy-document "file://${CREDENTIAL_HELPER_ROLE_PATH}" --query "Role.Arn" --output text)
 
     # Set the IAM role ARNs as GitHub Actions outputs
     echo "ecr-webhook-role-arn=${ECR_WEBHOOK_ROLE_ARN}" >> "$GITHUB_OUTPUT"
     echo "ecr-credential-helper-role-arn=${ECR_CREDENTIAL_HELPER_ROLE_ARN}" >> "$GITHUB_OUTPUT"
 
     # Attach policies to roles
-    aws iam attach-role-policy --role-name ecr-webhook --policy-arn "$ECR_WEBHOOK_POLICY_ARN"
-    aws iam attach-role-policy --role-name ecr-credential-helper --policy-arn "$ECR_CREDENTIAL_HELPER_POLICY_ARN"
+    aws iam attach-role-policy --role-name "$WEBHOOK_IAM_NAME" --policy-arn "$ECR_WEBHOOK_POLICY_ARN"
+    aws iam attach-role-policy --role-name "$CREDENTIAL_HELPER_IAM_NAME" --policy-arn "$ECR_CREDENTIAL_HELPER_POLICY_ARN"
 
     echo "IAM roles and policies created and attached successfully."
 }
 
 delete() {
     # Fetch policy ARNs
-    ECR_WEBHOOK_POLICY_ARN=$(aws iam list-policies --query "Policies[?PolicyName=='ecr-webhook'].Arn" --output text)
-    ECR_CREDENTIAL_HELPER_POLICY_ARN=$(aws iam list-policies --query "Policies[?PolicyName=='ecr-credential-helper'].Arn" --output text)
+    ECR_WEBHOOK_POLICY_ARN=$(aws iam list-policies --query "Policies[?PolicyName==${WEBHOOK_IAM_NAME}].Arn" --output text)
+    ECR_CREDENTIAL_HELPER_POLICY_ARN=$(aws iam list-policies --query "Policies[?PolicyName==${CREDENTIAL_HELPER_IAM_NAME}].Arn" --output text)
 
     # Detach policies from roles
-    aws iam detach-role-policy --role-name ecr-webhook --policy-arn "$ECR_WEBHOOK_POLICY_ARN"
-    aws iam detach-role-policy --role-name ecr-credential-helper --policy-arn "$ECR_CREDENTIAL_HELPER_POLICY_ARN"
+    aws iam detach-role-policy --role-name "$WEBHOOK_IAM_NAME" --policy-arn "$ECR_WEBHOOK_POLICY_ARN"
+    aws iam detach-role-policy --role-name "$CREDENTIAL_HELPER_IAM_NAME" --policy-arn "$ECR_CREDENTIAL_HELPER_POLICY_ARN"
 
     # Delete IAM policies
     aws iam delete-policy --policy-arn "$ECR_WEBHOOK_POLICY_ARN"
     aws iam delete-policy --policy-arn "$ECR_CREDENTIAL_HELPER_POLICY_ARN"
 
     # Delete IAM roles
-    aws iam delete-role --role-name ecr-webhook
-    aws iam delete-role --role-name ecr-credential-helper
+    aws iam delete-role --role-name "$WEBHOOK_IAM_NAME"
+    aws iam delete-role --role-name "$CREDENTIAL_HELPER_IAM_NAME"
 
     echo "IAM roles and policies detached and deleted successfully."
 }
