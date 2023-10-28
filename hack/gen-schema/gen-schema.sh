18ae877d729e483176f89c02e8685cfdf410af88
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2023-10-28 10:20:30 -0500

    Fix go run command in gen-schema.sh

--- hack/gen-schema/gen-schema.sh
+++ hack/gen-schema/gen-schema.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 # Create the json schema for the Zarf structs and use it to create the typescript definitions
-go run hack/gen-schema/main.go gen-schema | npx quicktype -s schema -o ./capabilities/zarf-types.ts
+go run hack/gen-schema/main.go | npx quicktype -s schema -o ./capabilities/zarf-types.ts
